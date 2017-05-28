d472e04a0647d4fd9cb267b3d2120851d2906dd1
Salim ABDELFETTAH <s.abd.fettah@gmail.com>
2017-05-28 13:56:12 +0200

    renaming

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'tcg-auto-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class TcgAutoComponent {
+  title = 'TCG Auto';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './app.component';
+import { TcgAutoComponent } from './app.component';
 
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MdButtonModule, MdCheckboxModule} from '@angular/material';
@@ -12,7 +12,7 @@ import 'hammerjs';
 
 @NgModule({
   declarations: [
-    AppComponent
+    TcgAutoComponent
   ],
   imports: [
     BrowserModule,
@@ -23,6 +23,6 @@ import 'hammerjs';
     MdCheckboxModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [TcgAutoComponent]
 })
-export class AppModule { }
+export class TcgAutoAppModule { }
